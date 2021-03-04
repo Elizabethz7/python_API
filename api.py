5f1d66ae50dbd7594792917ac6e82a785c73ffc0
Elizabethz7 <7721Elizabethz7@users.noreply.github.com>
2021-03-03 18:28:31 -0600

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,28 @@
 import flask #flask asigna solicitudes HTTP a funciones Python
+from flask import request, jsonify 
+#jsonify funcion que nos permite convertir listas y diccionarios a formato JSON
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+app = flask.Flask(__name__) #Crea el objeto de aplicación Flask
+app.config["DEBUG"] = True  # Inicia el depurador, si la aplicacion tiene un formato incorrecto se vera un error
 
 
 @app.route('/', methods=['GET']) #la ruta '/' la asignamos a una funcion home
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>PRUEBA</h1>"
 
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books) #book seria una lista
+@app.errorhandler(404) #si no existe la ruta
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+#login
+
+#album
+
+#fotos
+
+#user
+
+app.run() #Uno de los metodos de la aplicacion Flask
 app.run()
